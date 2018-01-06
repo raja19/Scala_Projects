4ec4f7e3dd08f200dd8892f0d8aa2cfb3a095403
ramesh <ramesh@localhost.localdomain>
2018-01-06 23:07:39 +0530

    second commit

--- sbt_project_example/src/main/scala/first_ex/number_ex.scala
+++ sbt_project_example/src/main/scala/first_ex/number_ex.scala
@@ -4,6 +4,6 @@ object number_ex extends App {
 
      val ages = Seq(42, 75, 29, 64)
      println(s"The oldest person is ${ages.max}")
-     println("working")
+     println("working changed")
 
 }
